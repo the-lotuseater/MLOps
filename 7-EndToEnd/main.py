7f094af98222d3dda2ed161320a9400888e64e63
Abhishek Birhade <abirhade@gatechu.edu>
2025-12-28 17:53:29 -0600

    Add DataIngestion

--- 7-EndToEnd/main.py
+++ 7-EndToEnd/main.py
@@ -1,2 +1,20 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
 logger.info('Welcome to my project.')
+def start_training_pipeline():
+    logger.info('Enter start training pipeline.')
+    STAGE_NAME='Data Ingestion Pipeline'
+    try:
+        logger.info(f'>>>> stage {STAGE_NAME} started <<<<')
+        obj = DataIngestionTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f'>>>> {STAGE_NAME} completed <<<<')
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    logger.info('Exit start training pipeline.')
+    
+if __name__=='__main__':
+    start_training_pipeline()

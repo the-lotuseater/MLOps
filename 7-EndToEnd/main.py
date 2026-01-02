7b74b0553f4f0571db1be9f9f161340bbad48d68
Abhishek Birhade <abirhade@gatechu.edu>
2026-01-02 17:24:28 -0600

    Complete Data validation step

--- 7-EndToEnd/main.py
+++ 7-EndToEnd/main.py
@@ -1,14 +1,21 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 logger.info('Welcome to my project.')
 def start_training_pipeline():
     logger.info('Enter start training pipeline.')
-    STAGE_NAME='Data Ingestion Pipeline'
     try:
+        STAGE_NAME='Data Ingestion Pipeline'
         logger.info(f'>>>> stage {STAGE_NAME} started <<<<')
         obj = DataIngestionTrainingPipeline()
         obj.initiate_data_ingestion()
+        logger.info(f'>>>> {STAGE_NAME} completed <<<<')
+
+        STAGE_NAME='Data Validation Pipeline'
+        logger.info(f'>>>> stage {STAGE_NAME} started <<<<')
+        obj = DataValidationTrainingPipeline()
+        obj.initiate_data_validation()
         logger.info(f'>>>> {STAGE_NAME} completed <<<<')
     except Exception as e:
         logger.exception(e)
